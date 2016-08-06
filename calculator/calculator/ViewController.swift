50daf6ea73a2301f6b584d0486c864d6044aab07
feliun <felipe.polo@gmail.com>
2016-08-06 18:19:22 +0100

    Some logic

--- calculator/calculator/ViewController.swift
+++ calculator/calculator/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     var currentOperation:Operator = Operator.nothing
     var calcState:CalculationState = CalculationState.enteringNum
+    var firstOperator = 0
+    var secondOperator = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,10 +25,46 @@ class ViewController: UIViewController {
     //button actions
     
     @IBAction func numberClicked (_ sender: UIButton) {
+        if (calcState == CalculationState.enteringNum) {
+            self.firstOperator = self.firstOperator * 10 + sender.tag
+            resultLabel.text = String(self.firstOperator)
+        } else if (calcState == CalculationState.newNumStarted) {
+            self.secondOperator = self.secondOperator * 10 + sender.tag
+            resultLabel.text = String(self.secondOperator)
+        }
     }
     @IBAction func operatorClicked (_ sender: UIButton) {
+        switch(sender.tag) {
+        case 10: self.currentOperation = Operator.add
+                    break
+        case 11: self.currentOperation = Operator.subtract
+            break
+        case 12: self.currentOperation = Operator.times
+            break
+        case 13: self.currentOperation = Operator.divide
+            break
+        default: self.currentOperation = Operator.nothing
+        }
+        self.calcState = CalculationState.newNumStarted
     }
     @IBAction func equalsClicked (_ sender: UIButton) {
+        var result = 0
+        switch(self.currentOperation) {
+        case Operator.add: result = self.firstOperator + self.secondOperator
+            break
+        case Operator.subtract: result = self.firstOperator - self.secondOperator
+            break
+        case Operator.times: result = self.firstOperator * self.secondOperator
+            break
+        case Operator.divide: result = self.firstOperator / self.secondOperator
+            break
+        default: break
+        }
+        resultLabel.text = String(result)
+        self.firstOperator = 0
+        self.secondOperator = 0
+        self.calcState = CalculationState.enteringNum
+        self.currentOperation = Operator.nothing
     }
 
     override func didReceiveMemoryWarning() {
